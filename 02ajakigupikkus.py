7479e8914576bb4e62e44419b7fb7edd0d6e97a7
Anneli Tikerber <annelit@hkhk.edu.ee>
2024-12-11 22:10:36 +0200

    uus fail

---
+++ 02ajakigupikkus.py
@@ -0,0 +1,16 @@
+# Autor: Anneli Tikerber
+# Kuupäev: 11. detsember 2024
+# Ülesanne: Aia pikkuse arvutamine
+
+# Küsime kasutajalt kahe aiaosa pikkused meetrites
+pikkus1 = float(input("Sisestage esimese aiaosa pikkus meetrites: "))
+pikkus2 = float(input("Sisestage teise aiaosa pikkus meetrites: "))
+
+# Arvutame aia kogupikkuse
+kogupikkus = 2 * (pikkus1 + pikkus2)
+
+# Väljastame tulemuse f-stringi abil
+print(f"Aia kogupikkus on {kogupikkus} meetrit.")
+    
+    
+ 
