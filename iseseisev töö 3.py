4624295c9ece8920ff6cd9462ccb68ed678958d3
Anneli172 <annelit@hkhk.edu.ee>
2025-01-30 16:30:36 +0200

    valmis

---
+++ iseseisev töö 3.py
@@ -0,0 +1,55 @@
+# fail = open("rebased.txt" , encoding="UTF-8")
+# vastuvõetud =[]
+
+# for rida in fail:
+#    # print(rida, end="")
+#     vastuvõetud.append(int(rida))
+
+
+# fail.close()
+
+# # aasta = 9
+# # print(vastuvõetud[aasta-1])
+
+# # aasta = input("Lisa aasta 2011-2019: ")
+# print(vastuvõetud[int(aasta[3])-1])
+
+# 3.3 Sissetulekud
+
+# fail = open("konto.txt", encoding="UTF-8")
+
+
+# for kirje in fail:
+
+#     if float(kirje) > 0:
+#         print(float(kirje), end="\n")
+
+# fail.close()
+
+# 3-4 Jukebox
+
+
+# Ada tahab valida plaadiautomaadist laulu ja uurib, milliseid laule masin mängib. Muusikapalad on kirjas failis, kus iga laul on eraldi real.
+# Programmi testimiseks kasutatakse järgmisi faile, mida võite salvestada või koostada ise mõne tekstiredaktoriga (nt Notepad):
+
+musa = "edm"
+
+fail = open(f"txt/{musa}.txt", encoding="UTF-8")
+
+
+nr = 1
+for pala in fail:
+    print(str(nr)+"."+pala, end="")
+    nr+=1
+ # Kuva valitud lugu   
+    print()
+    valik = int(input("Vali lugu: "))
+    fail = open(musa, encoding="UTF-8")
+    mangin = 1
+    for pala in fail:
+        if valik == mangin:
+         print(pala, end="")
+         mangin+=1
+
+
+fail.close
