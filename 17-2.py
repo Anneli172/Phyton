f963ddebf1897c02f8c72e6c7ab8cf93974d1d66
Anneli172 <annelit@hkhk.edu.ee>
2025-02-14 08:51:50 +0200

    Update 17-2.py

--- 17-2.py
+++ 17-2.py
@@ -1,4 +1,12 @@
+mpalgad = 0
+
 with open("palgad.txt") as fail:
     sisu = fail.readlines()
     for i in sisu:
-        print(i,end="")
+        #print(i,end="")
+       tykeldus = (i.split(","))
+       print(tykeldus[3])
+       if tykeldus[3]=="Mees":
+            mpalgad+=float(tykeldus[6])
+    print(f"Meeste palgad: {mpalgad:.2f}")
+
