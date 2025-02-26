7d145b21a2c1b404e3d4cf627e98315a2788f711
Anneli172 <anneli@hkhk.edu.ee>
2025-02-26 20:04:58 +0200

    valmis

--- harjutus books.py
+++ harjutus books.py
@@ -2,17 +2,21 @@ import json
 
 with open('books.json', 'r', encoding='utf-8') as file:
     books = json.load(file)
+    otsitav_aasta = int(input("Sisesta aasta, mille raamatut otsid:"))
+    if not any(book['published_year'] == otsitav_aasta for book in books):
+        print(f"Ei leitud raamatut {otsitav_aasta}.")
+
     # Iga raamatu andmete väljastamine:
     for book in books:
-        title = book['title']
-        author = book['author']
-        published_year = book['published_year']
-        copies_availlable = book['copies_available']
+        if book['published_year'] ==otsitav_aasta:
+            title = book['title']
+            author = book['author']
+            published_year = book['published_year']
+            copies_availlable = book['copies_available']
 
-        print(f"Pealkiri: {title}")
-        print(f"Autor:{author}")
-        print(f"Avaldamisaasta: {published_year}")
-        print(f"saada olevad koopiad:{copies_availlable}")
-        print("-"*40) # Visuaalne eraldaja
+            print(f"Pealkiri: {title}")
+            print(f"Autor:{author}")
+            print(f"Avaldamisaasta: {published_year}")
+            print(f"Saada olevad koopiad:{copies_availlable}")
+            print("-"*40) # Visuaalne eraldaja
 
-        
