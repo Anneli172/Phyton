cc7ce0c48106cd5508e4baca2062188cc4b30be6
Anneli172 <anneli@hkhk.edu.ee>
2025-03-07 13:45:06 +0200

    valmis

--- harjutus comments.py
+++ harjutus comments.py
@@ -4,16 +4,21 @@
 import json
 
 with open('comments.json', 'r', encoding='utf-8') as file:
-
-     comments = json.load(file)["comments"]
-     for comment in comments:
-        if "body" in comment:
-
-         if "username" in comment:
-            
+    comments = json.load(file)["comments"]
+    autor = input("Sisesta postitaja nimi:") 
+    leitud_kommentaarid = [comment for comment in comments if comment.get('user', {}).get('fullName') == autor]
     
-            print("--------Kommentaar-----")
-        print (comment["body"])
-        print (comment["id"])
-        print (comment["likes"])
-            
+    if not leitud_kommentaarid:
+        print(f"Ei leitud postitajat {autor}.")
+    else:
+        for comment in leitud_kommentaarid:
+            if "body" in comment:
+                # Kuvame kommentaari info
+                print("--------Kommentaar-----")
+                print(f"Kommentaari ID: {comment['id']}")
+                print(f"Kommentaari sisu: {comment['body']}")
+                print(f"Like'ide arv: {comment['likes']}")
+                print(f"Postitaja nimi: {comment['user']['fullName']}")
+                print(f"Postitaja kasutajanimi: {comment['user']['username']}")
+                print(f"Postitaja ID: {comment['user']['id']}")
+                print(f"Postitaja postituse ID: {comment['postId']}")
